3d4afdbdb0747233e7f611195f76114574fb00aa
1T Jobson <jobsonjgba@fab.mil.br>
2025-11-18 15:52:36 -0300

    refactor: aplica os conceitos de clean code

--- src/cliente.py
+++ src/cliente.py
@@ -1,14 +1,14 @@
 class Cliente:
-    def __init__(self, nome, telefone, email):
+    def __init__(self, nome: str, telefone: str, email: str):
         self.nome = nome
         self.telefone = telefone
         self.email = email
 
-    def __eq__(self, instance):
+    def __eq__(self, instance) -> bool:
         if not isinstance(instance, Cliente):
             return False
         return (
-            self.nome == instance.nome and
-            self.telefone == instance.telefone and
-            self.email == instance.email
+            self.nome == instance.nome
+            and self.telefone == instance.telefone
+            and self.email == instance.email
         )
