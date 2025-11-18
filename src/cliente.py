cf9b4af859b2d8b420c8f9dd4fdf3dca937774ac
1T Jobson <jobsonjgba@fab.mil.br>
2025-11-18 15:45:59 -0300

    feat: implementa a classe Cliente que armazenará informações do cliente

--- src/cliente.py
+++ src/cliente.py
@@ -1,2 +1,14 @@
 class Cliente:
-    pass
+    def __init__(self, nome, telefone, email):
+        self.nome = nome
+        self.telefone = telefone
+        self.email = email
+
+    def __eq__(self, instance):
+        if not isinstance(instance, Cliente):
+            return False
+        return (
+            self.nome == instance.nome and
+            self.telefone == instance.telefone and
+            self.email == instance.email
+        )
